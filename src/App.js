1a9b1661baa70bc138acfb8ba131cc305668af09
Gaeul Jun <gaeul7000@naver.com>
2025-06-12 22:35:36 +0900

    img import

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Dice from "./Dice";
+
+function App() {
+    return (
+        <div>
+            <Dice/>
+        </div>
+    );
+}
+
+// 다른데서 사용 가능하게 export
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <h1>안녕 리액트!</h1>,
-    document.getElementById('root')
-);
+
+root.render(<App />);
